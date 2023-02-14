4704e712e0148a25b42d821fc3acb26ed2a45228
lightmotive <7719153+lightmotive@users.noreply.github.com>
2023-02-14 03:51:19 +0000

    Complete assignment 06 exercises 1-4

---
+++ lesson2_schema_data_sql/06_working_with_a_single_table.sql
@@ -0,0 +1,32 @@
+CREATE TABLE
+  people (name varchar(255) NOT NULL, age smallint CHECK (age > 0), occupation varchar(255));
+
+INSERT INTO
+  people (name, age, occupation)
+VALUES
+  ('Abby', 34, 'biologist'),
+  ('Mu''nisah', 26, NULL),
+  ('Mirabelle', 40, 'contractor');
+
+SELECT
+  *
+FROM
+  people
+WHERE
+  name = 'Mu''nisah';
+
+SELECT
+  *
+FROM
+  people
+WHERE
+  occupation IS NULL;
+
+SELECT
+  *
+FROM
+  people
+LIMIT
+  1
+OFFSET
+  1;
