990dfcd0b650c54c4dfed56d4bbf8c57f4f258e5
lightmotive <7719153+lightmotive@users.noreply.github.com>
2023-02-15 03:17:13 +0000

    Complete assignment 06 exercises 8-11

--- lesson2_schema_data_sql/06_working_with_a_single_table.sql
+++ lesson2_schema_data_sql/06_working_with_a_single_table.sql
@@ -66,3 +66,44 @@ SELECT
   max(wingspan)
 FROM
   birds;
+
+CREATE TABLE
+  menu_items (
+    item text,
+    prep_time integer,
+    ingredient_cost numeric(4, 2),
+    sales integer,
+    menu_price numeric(4, 2)
+  );
+
+INSERT INTO
+  menu_items (item, prep_time, ingredient_cost, sales, menu_price)
+VALUES
+  ('omelette', 10, 1.50, 182, 7.99),
+  ('tacos', 5, 2.00, 254, 8.99),
+  ('oatmeal', 1, 0.50, 79, 5.99);
+
+SELECT
+  item,
+  menu_price - ingredient_cost AS profit
+FROM
+  menu_items
+ORDER BY
+  profit DESC
+LIMIT
+  1;
+
+SELECT
+  item,
+  labor_cost,
+  (menu_price - ingredient_cost - labor_cost) AS profit
+FROM
+  (
+    SELECT
+      item,
+      round((13.0 / 60.0) * prep_time, 2) AS labor_cost
+    FROM
+      menu_items
+  )
+ORDER BY
+  profit DESC;
