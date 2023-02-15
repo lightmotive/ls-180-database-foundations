0b21d2460101f14978ca848b1b1602dca8b67afc
lightmotive <7719153+lightmotive@users.noreply.github.com>
2023-02-15 03:40:51 +0000

    Complete assignment 07 exercise 1

---
+++ lesson2_schema_data_sql/07_loading_database_dumps.sql
@@ -0,0 +1,21 @@
+-- Ran commands in this directory:
+-- createdb test
+-- psql -d test < films1.sql
+-- ***
+-- films1.sql drops a `films` table if it exists, then creates a `films` table,
+-- then inserts data into that table.
+-- ***
+-- Command output:
+-- NOTICE:  table "films" does not exist, skipping
+--  - A helpful message!
+-- DROP TABLE
+--  - Though the table didn't exist per the previous message, this indicates
+--    the DROP TABLE command completed successfully.
+-- CREATE TABLE
+--  - The CREATE TABLE films... command completed successfully.
+--  - The following messages indicate, 3 times, that 1 row was inserted into oid 0:
+-- INSERT 0 1
+-- INSERT 0 1
+-- INSERT 0 1
+-- ***
+-- Complete remaining exercises...
