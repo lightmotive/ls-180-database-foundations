2baf49f622f5522dad991753235292d25917d28f
lightmotive <7719153+lightmotive@users.noreply.github.com>
2023-02-24 03:52:08 +0000

    Complete Lesson 3, assignment 6, exercises 5-7

--- lesson3_relational_data_and_joins/06_working_with_multiple_tables.sql
+++ lesson3_relational_data_and_joins/06_working_with_multiple_tables.sql
@@ -21,3 +21,41 @@ SELECT
 FROM
   customers AS c
   LEFT JOIN tickets AS t ON c.id = t.customer_id;
+
+SELECT
+  e.name,
+  count(t.event_id) AS popularity
+FROM
+  events AS e
+  LEFT OUTER JOIN tickets AS t ON e.id = t.event_id
+GROUP BY
+  e.name
+ORDER BY
+  popularity DESC;
+
+SELECT
+  c.id,
+  c.email,
+  count(DISTINCT t.event_id) AS event_count
+FROM
+  customers AS c
+  INNER JOIN tickets AS t ON c.id = t.customer_id
+GROUP BY
+  c.id
+HAVING
+  count(DISTINCT t.event_id) = 3;
+
+SELECT
+  e.name AS "event",
+  e.starts_at,
+  sc.name AS section,
+  st.row,
+  st.number AS seat
+FROM
+  customers as c
+  INNER JOIN tickets AS t ON c.id = t.customer_id
+  INNER JOIN events AS e ON e.id = t.event_id
+  INNER JOIN seats AS st ON st.id = t.seat_id
+  INNER JOIN sections AS sc ON sc.id = st.section_id
+WHERE
+  c.email = 'gennaro.rath@mcdermott.co';
